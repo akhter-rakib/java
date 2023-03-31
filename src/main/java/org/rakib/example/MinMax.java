1978a81b39cda227ef5112d4b1a3225480150e2b
farzana-yesmin <farzana_cse15@yahoo.com>
2023-04-01 03:06:29 +0600

    MinMax

---
+++ src/main/java/org/rakib/example/MinMax.java
@@ -0,0 +1,22 @@
+package org.rakib.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MinMax {
+    @Test
+    public void min() {
+        List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+        Integer minValue = numbers.stream().min(Comparator.naturalOrder()).get();
+        System.out.println(minValue);
+    }
+
+    @Test
+    public void max() {
+        List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+        Integer minValue = numbers.stream().max(Comparator.naturalOrder()).get();
+        System.out.println(minValue);
+    }
+}
