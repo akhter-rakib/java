180696f70c66a1b50ef7fb8ba830b3d1bd3b0192
farzana-yesmin <farzana_cse15@yahoo.com>
2023-04-26 23:52:12 +0600

    frequency of each character from a given String

--- src/main/java/org/rakib/interview/FrequencyCharacter.java
+++ src/main/java/org/rakib/interview/FrequencyCharacter.java
@@ -1,5 +1,7 @@
 package org.rakib.interview;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,21 +12,23 @@ import static java.util.stream.Collectors.groupingBy;
 public class FrequencyCharacter {
     /*Using stream find frequency of each character in a given String ?
      also try to do without stream ?*/
-    public static void main(String[] args) {
+    @Test
+    void frequencyOfEachCharUsingStream() {
         String input = "Basanta";
         Map<Character, Long> collect = input.chars()
                 .mapToObj(value -> (char) value)
                 .filter(Character::isLetterOrDigit)
                 .collect(groupingBy(identity(), counting()));
         System.out.println(collect);
+    }
 
-        /*without stream*/
+    @Test
+    void frequencyOfEachChar() {
+        String input = "Basanta";
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            if (Character.isLetterOrDigit(c)) {
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            }
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         System.out.println(map);
     }
